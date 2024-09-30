4fce0ac28bf677cd9ff5d7ac42353c447dcc3f5b
stepanpigriteus <avetor-sport@yandex.ru>
2024-09-30 20:34:32 +0500

    First in the bus

---
+++ src/components/container/container.tsx
@@ -0,0 +1,14 @@
+
+
+export default function Container() {
+
+    return(
+        <>
+         <div className="task_container">
+            <div className="stack">
+                
+            </div>
+        </div>
+        </>
+    )
+}
---
+++ src/components/task_item/task_item.tsx
@@ -0,0 +1,9 @@
+
+
+export default function TaskItem() {
+    return(
+        <>
+       
+        </>
+    )
+}

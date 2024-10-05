039765e535d42395c3c226b7e58753e8826ec19c
stepanpigriteus <avetor-sport@yandex.ru>
2024-10-05 11:03:37 +0500

    Первый коммит

--- src/components/container/tasks cont/task_list_cont.tsx
+++ src/components/container/tasks cont/task_list_cont.tsx
@@ -4,7 +4,7 @@ import TaskItem from "../../task_item/task_item";
 export default function TaskList() {
     return(
         <>
-        <section className="min-w-[30%] max-w-[45%] m-0 min-h-[478px] px-4 py-4   shadow-md bg-[rgba(15,33,63,0.8)] rounded-l-lg ">
+        <section className="min-w-[30%] max-w-[45%] m-0 min-h-[478px] px-4 py-4  shadow-md bg-[rgba(15,33,63,0.8)] rounded-l-lg ">
         <ul>
         <TaskItem/>
         <TaskItem/>
